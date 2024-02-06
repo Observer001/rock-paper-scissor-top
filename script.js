f37efd0ec458800245a5baccbc6b11c25fd4089d
Observer001 <46171900+Observer001@users.noreply.github.com>
2024-02-06 09:59:05 +0530

    fix: playGame function to fix game logic and update UI ti display in table

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-let options = ["rock", "paper", "scissor"]
+const options = ["rock", "paper", "scissor"]
+
+const input = document.querySelector('input');
+const button = document.querySelector('#total');
 
 function getComputerChoice() {
     return options[Math.floor(Math.random() * options.length)];
@@ -8,30 +11,58 @@ function playRound(playerSelection, computerSeletion) {
     playerSelection = playerSelection.toLowerCase();
     computerSeletion = computerSeletion.toLowerCase();
 
-    if(playerSelection == computerSeletion)
-        return `Tie! Both have chosen ${playerSelection}`
+    if(playerSelection === computerSeletion)
+        return 'Tie!'
     else if(
-        (playerSelection == "rock" && computerSeletion == "paper") ||
-        (playerSelection == "paper" && computerSeletion == "scissor") ||
-        (playerSelection == "scissor" && computerSeletion == "rock")
+        (playerSelection === "rock" && computerSeletion === "paper") ||
+        (playerSelection === "paper" && computerSeletion === "scissor") ||
+        (playerSelection === "scissor" && computerSeletion === "rock")
     )   {
-        return `You Lose! ${computerSeletion} beats ${playerSelection}`
+        return 'computer'
     }
     else
-        return `You win! ${playerSelection} beats ${computerSeletion}`
-
+        return 'player'
 }
 
 function playGame() {
-    let count = 5;
-    let wins = 0;
-    let loss = 0;
+    let totalPoints = +input.value;
+    let player = 0;
+    let comp = 0;
 
-    for(let i=0; i < 5; i++) {
-        let playerSelection = prompt("Enter your choice: ")
-        let computerSeletion = getComputerChoice()
-        console.log(playRound(playerSelection, computerSeletion))        
-    }
+    button.addEventListener('click', () => {        
+        input.value = '';        
+    
+        const playerChoice = document.querySelector('#player-choice');
+        const compChoice = document.querySelector('#comp-choice');
+        const playerPoint = document.querySelector('#player-point');
+        const compPoint = document.querySelector('#comp-point');
+
+        playerChoice.innerText = 'blank';
+        compChoice.innerText = 'blank';
+        playerPoint.innerText = '0';
+        compPoint.innerText = '0';    
+
+        const btns = document.querySelectorAll('.card');
+        btns.forEach((btn) => {
+            btn.addEventListener('click', () => {
+                let playerSelection = btn.innerText;
+                let computerSelection = getComputerChoice();
+
+                playerChoice.innerText = playerSelection;
+                compChoice.innerText = computerSelection;
+
+                let roundWinner = playRound(playerSelection, computerSelection);
+                if(roundWinner === 'player') playerPoint.innerText = ++player;
+                if(roundWinner === 'computer') compPoint.innerText = ++comp;
+                
+                if(player === totalPoints) alert('Player wins!');
+                else if(comp === totalPoints) alert('Computer wins!');
+
+            });
+        });        
+    });
+
 }
+
 
 playGame();
